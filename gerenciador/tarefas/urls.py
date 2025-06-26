7987be5e3ff41d7a15c45b98f7097f17bc21d9bf
Leonardo Ribeiro <leonardo.rbr@icloud.com>
2025-06-25 20:55:28 -0400

    Atualização - Função de editar tarefa e projeto

--- gerenciador/tarefas/urls.py
+++ gerenciador/tarefas/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('projetos/novo/', views.cadastrar_projeto, name='cadastrar_projeto'),
     path('tarefas/', views.lista_tarefas, name='lista_tarefas'),
     path('tarefas/nova/', views.cadastrar_tarefa, name='cadastrar_tarefa'),
+    path('projetos/editar/<int:id>/', views.editar_projeto, name='editar_projeto'),
+    path('tarefas/editar/<int:id>/', views.editar_tarefa, name='editar_tarefa'),
 ]
--- gerenciador/tarefas/views.py
+++ gerenciador/tarefas/views.py
@@ -1,6 +1,7 @@
 # tarefas/views.py
 
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Projeto, Tarefa
 from .forms import ProjetoForm, TarefaForm
 
@@ -25,3 +26,21 @@ def cadastrar_tarefa(request):
         form.save()
         return redirect('lista_tarefas')
     return render(request, 'tarefas/form_tarefa.html', {'form': form})
+
+def editar_projeto(request, id):
+    projeto = Projeto.objects.get(id=id)
+    form = ProjetoForm(request.POST or None, instance=projeto)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Projeto atualizado com sucesso!')
+        return redirect('lista_projetos')
+    return render(request, 'tarefas/form_projeto.html', {'form': form, 'editar': True})
+
+def editar_tarefa(request, id):
+    tarefa = Tarefa.objects.get(id=id)
+    form = TarefaForm(request.POST or None, instance=tarefa)
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Tarefa atualizada com sucesso!')
+        return redirect('lista_tarefas')
+    return render(request, 'tarefas/form_tarefa.html', {'form': form, 'editar': True})
